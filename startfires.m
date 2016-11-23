4f48b3fe7705b589aebb6ac0525da31b2870bc2c
cobysdavis <cobysdavis@yahoo.ca>
2016-11-23 17:21:21 -0500

    last

--- startfires.m
+++ startfires.m
@@ -5,8 +5,8 @@ ypos=zeros(F,1);
 startpos=zeros(F,2);
 i=0;
 for i=1:F
-    xpos(i)=randi([1 matrixDim]);
-    ypos(i)=randi([1 matrixDim]);
+    xpos(i)=randi([floor(matrixDim/5) floor(matrixDim*4/5)]);
+    ypos(i)=randi([floor(matrixDim/5) floor(matrixDim*4/5)]);
     startpos(i,:)=[xpos(i) ypos(i)];
     firepositions(xpos(i),ypos(i))=1;
 end
